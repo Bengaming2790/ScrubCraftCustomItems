60dff026f634491de705801eef476fdf599ce9d3
Regular Ben <80541274+Bengaming2790@users.noreply.github.com>
2024-10-08 09:18:30 -0400

    Update Keys.java

--- ScrubCustomItems/src/main/ca/techgarage/ScrubCustomItems/Keys.java
+++ ScrubCustomItems/src/main/ca/techgarage/ScrubCustomItems/Keys.java
@@ -39,8 +39,8 @@ public class Keys {
 	public static final NamespacedKey SHULKER_SCYTHE = new NamespacedKey(Main.getInstance(), "ShulkerScythe");	
 	public static final NamespacedKey QUEST_BOOK_KEY = new NamespacedKey(Main.getInstance(), "QuestBook");
 	public static final NamespacedKey HEAVY_SCYTHE = new NamespacedKey(Main.getInstance(), "HeavyScythe");
+	public static final NamespacedKey SWIFT_SCYTHE = new NamespacedKey(Main.getInstance(), "SwiftScythe");
 
-	
 	public static final NamespacedKey FREEZE_CLOCK = new NamespacedKey(Main.getInstance(), "FreezeClock");
 	public static final NamespacedKey BACK_PACK_ID = new NamespacedKey(Main.getInstance(), "BackPack");
 
